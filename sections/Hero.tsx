ec83f8a3c74f833b3d6b29def095a995a8a3a5c7
jagxman <jagjitmangar177@gmail.com>
2023-08-21 23:20:14 -0400

    tailwind config update w/ ShoeCard Updated

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,12 +1,23 @@
+"use client";
+
 import Button from "@/components/Button";
-import { statistics } from "@/constants";
+import ShoeCard from "@/components/ShoeCard";
+import { statistics, shoes } from "@/constants";
 import { arrowRight } from "@/public/assets/icons";
+import { bigShoe1 } from "@/public/assets/images";
+import { useState } from "react";
+import Image from "next/image";
 
 const Hero = () => {
+
+
+  const [bigShoeImg, setBigShoeImg] = useState(bigShoe1);
+
+
   return (
     <section
       id="home"
-      className="w-full border-2 flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
       <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28 ">
         <p className="text-xl font-montserrat text-coral-red">
@@ -32,7 +43,27 @@ const Hero = () => {
           {statistics.map((stat) => (
             <div key={stat.label}>
               <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
-              <p className="leading-7 font-montserrat text-slate-gray">{stat.label}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
+        <Image
+          src={bigShoeImg}
+          alt="shoe1"
+          width={610}
+          height={502}
+          className="object-contain relative z-10"
+        />
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+          {shoes.map((shoe) => ( 
+            <div key={shoe.thumbnail}>
+              <ShoeCard imgURL={shoe} changeBigShoe={(shoe:any) => {setBigShoeImg(shoe)}} bigShoeImg={bigShoeImg} />
             </div>
           ))}
         </div>
