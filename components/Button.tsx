7ef12c658b0ecd6041c3c92382046c2a2f6df72e
jagxman <jagjitmangar177@gmail.com>
2023-08-22 02:24:38 -0400

    Special Offer + Custom Button Component Update

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,21 +1,34 @@
-
-import Image from "next/image"
-
+import Image from "next/image";
 
 interface buttonProps {
-    label: string;
-    iconURL?: string;
-
+  label: string;
+  iconURL?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+  textColor?: string;
 }
 
-
-const Button = ( {label, iconURL} : buttonProps) => {
+const Button = ({
+  label,
+  iconURL,
+  backgroundColor,
+  borderColor,
+  textColor,
+}: buttonProps) => {
   return (
-    <button className="flex justify-center items-center gap-2 px-7 py-4 font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
-        {label}
-        { iconURL && <Image src={iconURL} alt="icon" className="mt-2 rounded-full w-5 h-5"/>}
+    <button
+      className={`flex justify-center items-center gap-2 px-7 py-4 font-montserrat border text-lg leading-none rounded-full ${
+        backgroundColor
+          ? `${backgroundColor} ${borderColor} ${textColor}`
+          : `bg-coral-red  text-white border-coral-red`
+      }`}
+    >
+      {label}
+      {iconURL && (
+        <Image src={iconURL} alt="icon" className="mt-2 rounded-full w-5 h-5" />
+      )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
